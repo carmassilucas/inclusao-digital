8d7b16b6f1d81c6d29870427308ebbc4fb81a410
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-26 20:22:35 -0300

    base do projeto gerado pelo spring initializr

---
+++ src/main/java/br/com/ifsp/aluno/inclusaodigital/InclusaoDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.aluno.inclusaodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclusaoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InclusaoDigitalApplication.class, args);
+	}
+
+}
